a6fe648d790f1c713b0bfee15b344cd7e48d6a9a
begosustatx <32163172+begosustatx@users.noreply.github.com>
2017-11-04 13:10:27 +0100

    bukatuta

--- myquiz/php/showXMLQuestions.php
+++ myquiz/php/showXMLQuestions.php
@@ -1,3 +1,11 @@
 <?php
+	$xml= simplexml_load_file("../xml/questions.xml");
+	echo '<table border=1><tr><th> Testua </th><th> Zailtasuna </th><th> Gaiarloa </th></tr>';
+	foreach($xml->assessmentItem as $question){
+		echo '<tr><td>' . $question->itemBody->p . '</td><td>' . $question['complexity'] . '</td><td>' . $question['subject'] . '</td></tr>';
+		/*echo("\t\t");
+		echo("\t\t".); 
+		echo("\t\t".$question->itemBody->p."<br/>");	*/
+	}
 	
 ?>
