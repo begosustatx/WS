70a2ce7f1f8a5634f1a11707beb3953419201728
aps54 <31988318+aps54@users.noreply.github.com>
2017-11-10 13:49:41 +0100

    Lab5

--- myquiz/php/showQuestionsAJAX.php
+++ myquiz/php/showQuestionsAJAX.php
@@ -7,12 +7,9 @@
 	}
 	
 	$galderak=mysqli_query($link, "select * from questions");
-	echo '<table border=1><tr><th> Posta </th><th> Testua </th><th> Erantzun zuzena </th><th> Erantzun okerra 1 </th><th> Erantzun okerra 2 </th><th> Erantzun okerra 3 </th><th> Zailtasuna </th><th> Gaiarloa </th> <th> Argazkia </th></tr>';
+	echo '<table border=1><tr><th> Testua </th><th> Zailtasuna </th><th> Gaiarloa </th></tr>';
 	while($row = mysqli_fetch_array($galderak)){
-		if($row['argazkia']==NULL){
-			echo '<tr><td>' . $row['posta'] . '</td><td>' . $row['testua'] . '</td><td>' . $row['eZuzen'] . '</td><td>' . $row['eOker1'] . '</td><td>' . $row['eOker2'] . '</td><td>' . $row['eOker3'] . '</td><td>' . $row['zailtasun'] . '</td><td>' . $row['gaiarloa'] . '</td><td> <img src="../img/default.jpg" width="300px" height="300px" /></td></tr>';
-		} else
-			echo '<tr><td>' . $row['posta'] . '</td><td>' . $row['testua'] . '</td><td>' . $row['eZuzen'] . '</td><td>' . $row['eOker1'] . '</td><td>' . $row['eOker2'] . '</td><td>' . $row['eOker3'] . '</td><td>' . $row['zailtasun'] . '</td><td>' . $row['gaiarloa'] . '</td><td> <img src="data:' . $row['arg_mota'] . ';base64,' . base64_encode($row['argazkia']) . '" width="300px" height="300px" /></td></tr>';
+		echo '<tr><td>' . $row['testua'] . '</td><td>' . $row['zailtasun'] . '</td><td>' . $row['gaiarloa'] . '</td><td></tr>';
 	}	
 	echo '</table>';
 	mysqli_close($link); // Konexioa itxi
