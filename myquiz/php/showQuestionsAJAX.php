80a43cf3a85d3b195d26a0dcdf17c7e95ca3044b
aps54 <31988318+aps54@users.noreply.github.com>
2017-11-10 13:39:05 +0100

    Lab5

---
+++ myquiz/php/showQuestionsAJAX.php
@@ -0,0 +1,19 @@
+<?php
+	include 'dbconfig.php';
+	$link = mysqli_connect($server, $user, $pass, $db); // Konexioa ireki
+	if (mysqli_connect_errno()){
+		echo "Konexio hutxegitea MySQLra: " . mysqli_connect_error();
+		exit();
+	}
+	
+	$galderak=mysqli_query($link, "select * from questions");
+	echo '<table border=1><tr><th> Posta </th><th> Testua </th><th> Erantzun zuzena </th><th> Erantzun okerra 1 </th><th> Erantzun okerra 2 </th><th> Erantzun okerra 3 </th><th> Zailtasuna </th><th> Gaiarloa </th> <th> Argazkia </th></tr>';
+	while($row = mysqli_fetch_array($galderak)){
+		if($row['argazkia']==NULL){
+			echo '<tr><td>' . $row['posta'] . '</td><td>' . $row['testua'] . '</td><td>' . $row['eZuzen'] . '</td><td>' . $row['eOker1'] . '</td><td>' . $row['eOker2'] . '</td><td>' . $row['eOker3'] . '</td><td>' . $row['zailtasun'] . '</td><td>' . $row['gaiarloa'] . '</td><td> <img src="../img/default.jpg" width="300px" height="300px" /></td></tr>';
+		} else
+			echo '<tr><td>' . $row['posta'] . '</td><td>' . $row['testua'] . '</td><td>' . $row['eZuzen'] . '</td><td>' . $row['eOker1'] . '</td><td>' . $row['eOker2'] . '</td><td>' . $row['eOker3'] . '</td><td>' . $row['zailtasun'] . '</td><td>' . $row['gaiarloa'] . '</td><td> <img src="data:' . $row['arg_mota'] . ';base64,' . base64_encode($row['argazkia']) . '" width="300px" height="300px" /></td></tr>';
+	}	
+	echo '</table>';
+	mysqli_close($link); // Konexioa itxi
+	?>
