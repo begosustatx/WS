9e930089442e0aae4a87a1333727c68a4f98a035
begosustatx <32163172+begosustatx@users.noreply.github.com>
2017-11-11 19:54:50 +0100

    Lab5 ea bukatuta

--- myquiz/php/handlingQuizes.php
+++ myquiz/php/handlingQuizes.php
@@ -47,26 +47,61 @@
    </script>
 <script type="text/javascript" language = "javascript">
 
-		xhro = new XMLHttp q Re uest();
-		xhro.onreadystatechange = function(){
-			alert(xhro.readyState);
-			if ((xhro.readyState==4)&&(xhttp.status==200 ))
-			{ document.getElementById("galderakIkusi").innerHTML= xhro.responseText;}
-			}
 		function galderakIkusi(){
-			xhro.open("GET","showQuestionsAJAX.php"?testua=+str&zailtasun=+str&gaiarloa=+str&, true);
+			var xhro = new  XMLHttpRequest;
+			var testua=document.getElementById('testua').value;
+			var gaiarloa=document.getElementById('gaiarloa').value;
+			var zailtasun=document.getElementById('zailtasun').value;
+			var eZuzen=document.getElementById('eZuzen').value;
+			var eOker1=document.getElementById('eOker1').value;
+			var eOker2=document.getElementById('eOker2').value;
+			var eOker3=document.getElementById('eOker3').value;
+			ruta="showQuestionsAJAX.php";
+			envio1="testua="+testua;
+			envio2="gaiarloa="+gaiarloa;
+			envio3="zailtasun="+zailtasun;
+			envio4="eZuzen="+eZuzen;
+			envio5="eOker1="+eOker1;
+			envio6="eOker2="+eOker2;
+			envio7="eOker3="+eOker3;
+			url=ruta+"?"+envio1+"&"+envio2+"&"+envio3+"&"+envio4+"&"+envio5+"&"+envio6+"&"+envio7;
+			xhro.onreadystatechange = function(){
+				alert(xhro.readyState);
+				if ((xhro.readyState==4)&&(xhro.status==200 )){
+					document.getElementById("galderakIkusi").innerHTML= xhro.responseText;
+				}
+			};
+			console.log("Funtzioaren barruan");
+			xhro.open("GET",url, true);
 			xhro.send();
 		}
-
-		xhro.onreadystatechange = function(){
-			alert(xhro.readyState);
-			if ((xhro.readyState==4)&&(xhttp.status==200 ))
-			{ document.getElementById("galderaGehitu").innerHTML= xhro.responseText;}
-			}
-		function galderaGehitu(){
-			xhro.open("GET","addQuestionAJAX.php?"testua=+str&eZuzen==+str&eOker1=+str&eOker2=+str&eOker3=+str&zailtasun=+str&gaiarloa=+str, true);
+	function galderaGehitu(){
+			var xhro = new  XMLHttpRequest;
+			var testua=document.getElementById('testua').value;
+			var gaiarloa=document.getElementById('gaiarloa').value;
+			var zailtasun=document.getElementById('zailtasun').value;
+			var eZuzen=document.getElementById('eZuzen').value;
+			var eOker1=document.getElementById('eOker1').value;
+			var eOker2=document.getElementById('eOker2').value;
+			var eOker3=document.getElementById('eOker3').value;
+			ruta="addQuestionAJAX.php";
+			envio1="testua="+testua;
+			envio2="gaiarloa="+gaiarloa;
+			envio3="zailtasun="+zailtasun;
+			envio4="eZuzen="+eZuzen;
+			envio5="eOker1="+eOker1;
+			envio6="eOker2="+eOker2;
+			envio7="eOker3="+eOker3;
+			url=ruta+"?"+envio1+"&"+envio2+"&"+envio3+"&"+envio4+"&"+envio5+"&"+envio6+"&"+envio7;
+			xhro.onreadystatechange = function(){
+				alert(xhro.readyState);
+				if ((xhro.readyState==4)&&(xhro.status==200 )){
+					document.getElementById("galderaGehitu").innerHTML= xhro.responseText;
+				}
+			};
+			xhro.open("GET",url, true);
 			xhro.send();
-		}
+	}
 </script>
    <?php
 		$posta=$_GET['email'];
@@ -83,7 +118,7 @@
 		<nav class='main' id='n1' role='navigation'>
 			<span><a href='layoutR.php?email=<?php echo $posta;?>'>Home</a></span>
 			<span><a href='/quizzes'>Quizzes</a></span>
-			<span><a href='addQuestionWithImage.php?email=<?php echo $posta;?>'>Add Question</a></span>
+			<span><a href='addQuestionWithImage.php?email=<?php echo $posta;?>'>Handle a Quizz</a></span>
 			<span><a href='showQuestionsWithImages.php?email=<?php echo $posta;?>'>Show Questions</a></span>
 			<span><a href='creditsR.php?email=<?php echo $posta;?>'>Credits</a></span>
 		</nav>
@@ -128,99 +163,5 @@
 			<a href='https://github.com/begosustatx/WS'>Link GITHUB</a>
 		</footer>
 		
- 
-<?php
-	
-	if(isset($_POST['bidali']))
-	{	
-		if(isset($posta) && empty($posta)){
-			echo "<script> alert('Berrigorrezko eremu guztiak bete')</script>";
-		} elseif (empty($_POST['eZuzen']) || empty($_POST['eOker1']) || empty($_POST['eOker2']) || empty($_POST['eOker3']) || empty($_POST['zailtasun']) || empty($_POST['gaiarloa'])){
-			echo "<script> alert('Berrigorrezko eremu guztiak bete')</script>";
-		}	else {
-			$patroia='/^[a-z]{2,}[0-9]{3}@ikasle\.ehu\.(eus|es)$/';
-			if(!preg_match($patroia,$posta)){
-				echo "<script> alert('Posta okerra')</script>";
-			}
-			if(!isset($_POST['testua'])){
-				echo "<script> alert('Berrigorrezko eremu guztiak bete')</script>";
-			} else {
-				$testua= $_POST['testua'];
-				if(strlen($testua)<9){
-					echo "<script> alert('Galderaren testu motzegia')</script>";
-				}  
-			}
-		} 
-		
-		// Balio egokiak sartu badira XML fitxategia kargatu.
-		$xml = simplexml_load_file('../xml/questions.xml'); 
-	
-		include 'dbconfig.php';
-		$link = mysqli_connect($server, $user, $pass, $db); // Konexioa ireki
-		if (mysqli_connect_errno()){
-			echo "Konexio hutxegitea MySQLra: " . mysqli_connect_error();
-			exit();
-		}
-			
-		$onartuak = array("image/jpg", "image/jpeg", "image/gif", "image/png");
-		if(in_array($_FILES['argazkia']['type'], $onartuak)){ // Argazkia igo dela konprobatzeko
-			$img_tmp = $_FILES['argazkia']['tmp_name']; // Argazkiaren PATH.
-			$mota = $_FILES['argazkia']['type']; // Argazkiaren mota.
-			$imgData = mysqli_escape_string($link, file_get_contents($img_tmp));
-			$sql="INSERT INTO questions(posta, testua, eZuzen, eOker1, eOker2, eOker3, zailtasun, gaiarloa, argazkia, arg_mota) VALUES ('$posta', '$_POST[testua]', '$_POST[eZuzen]', '$_POST[eOker1]', '$_POST[eOker2]', '$_POST[eOker3]', '$_POST[zailtasun]', '$_POST[gaiarloa]','$imgData', '$mota')";
-			$ema = mysqli_query($link, $sql);
-			if(!$ema){
-				echo "Errorea query-a gauzatzerakoan: " . mysqli_error($link);
-				echo "<a href='../html/addQuestionHTML5.html'>Berriro saiatu</a>";
-			}else {
-				$nquestion = $xml->addChild('assessmentItem');
-				$nquestion->addAttribute('complexity', $_POST['zailtasun']);
-				$nquestion->addAttribute('subject', $_POST['gaiarloa']);
-				$text = $nquestion->addChild('itemBody');
-				$text->addChild('p', $_POST['testua']);
-				$qC = $nquestion->addChild('correctResponse');
-				$qC->addChild('value', $_POST['eZuzen']);
-				$qIs = $nquestion->addChild('incorrectResponses');
-				$qIs->addChild('value', $_POST['eOker1']);
-				$qIs->addChild('value', $_POST['eOker2']);
-				$qIs->addChild('value', $_POST['eOker3']);
-				$xml->asXML('../xml/questions.xml'); // Aldaketak XML fitxategian gorde
-				echo  "<br><p> Ondo txertatu da.</p>";
-				echo  "<p> Galdera guztiak ikusi ditzazkezu <a href='showQuestionsWithImages.php'>hemen</a></p>" ;
-			}
-		} else { // Argazkia ez bada sartzen
-			$sql="INSERT INTO questions(posta, testua, eZuzen, eOker1, eOker2, eOker3, zailtasun, gaiarloa) VALUES ('$posta', '$testua', '$_POST[eZuzen]', '$_POST[eOker1]', '$_POST[eOker2]', '$_POST[eOker3]', '$_POST[zailtasun]', '$_POST[gaiarloa]')";
-				
-			$ema = mysqli_query($link, $sql);
-			if(!$ema){
-				echo "Errorea query-a gauzatzerakoan: " . mysqli_error($link);
-				echo "<a href='../html/addQuestionHTML5.html'>Berriro saiatu</a>";
-			}else {
-				$nquestion = $xml->addChild('assessmentItem');
-				$nquestion->addAttribute('complexity', $_POST['zailtasun']);
-				$nquestion->addAttribute('subject', $_POST['gaiarloa']);
-				$text = $nquestion->addChild('itemBody');
-				$text->addChild('p', $_POST['testua']);
-				$qC = $nquestion->addChild('correctResponse');
-				$qC->addChild('value', $_POST['eZuzen']);
-				$qIs = $nquestion->addChild('incorrectResponses');
-				$qIs->addChild('value', $_POST['eOker1']);
-				$qIs->addChild('value', $_POST['eOker2']);
-				$qIs->addChild('value', $_POST['eOker3']);
-				$xml->asXML('../xml/questions.xml'); // Aldaketak XML fitxategian gorde
-				echo  "<br><p> Ondo txertatu da.</p>";
-				echo  "<p> Galdera guztiak ikusi ditzazkezu <a href='showXMLQuestions.php'>hemen</a></p>" ;
-			}
-		}
-		mysqli_close($link); // Konexioa itxi
-	}
-	/* 
-	function test_input($data) {
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}*/
-?>
  </body>
 </html>
