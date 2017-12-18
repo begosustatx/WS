0ac62148270fd3b8597e3950691fb0913099861f
begosustatx <32163172+begosustatx@users.noreply.github.com>
2017-12-18 16:42:57 +0100

    Aukerarik ez bada aukeratzen

--- myquiz/php/playbysubject.php
+++ myquiz/php/playbysubject.php
@@ -31,90 +31,97 @@
 	</nav>
 <?php
 	segurtasunaAnonimo();
+	
 	$gaia = $_GET['subject'];
-	include "dbconfig.php"; 
-	$link = mysqli_connect($server, $user, $pass, $db) or die ("Error while connecting to data base.");
-	$puntuazioa = $_GET['puntuazioa'];
-	//Aurreko galderan (egon bada) erantzun zuzena aukeratu bada, puntuazioari bat gehituko diogu
-	if($puntuazioa==1){
-		$link = mysqli_connect($server, $user, $pass, $db) or die ("Error while connecting to data base.");
-		$nick=$_SESSION['nick'];
-		$_SESSION['zuzenak'] = $_SESSION['zuzenak'] + 1;
-		$sql="UPDATE anonimoak SET puntuazioa=puntuazioa+1 WHERE nick='$nick'";
-		$result=mysqli_query($link, $sql);
-		if(!($result))
-			echo "Error in the query" . $result->error;
+	if(($gaia)=='Select a subject'){ 
+		echo "<script> alert('Aukeraren bat aukeratu behar duzu ');</script>";
+		echo "<script> window.location.assign('quizesbysubject.php');</script>";
 	}
+	else{
 	
-	//Galdera guztien id-ak lortu eta array batean sartuko ditugu
-	$sql="SELECT ID FROM questions WHERE gaiarloa='$gaia'";
-	$result=mysqli_query($link, $sql);
-	
-	
-	if(!($result)){
-		echo "Error in the query" . $result->error;
-	} else {
-		$rows_cont = $result->num_rows;
-		if($rows_cont==0){
-			echo "<script> alert('Ez dago galderarik');</script>";}
-		else{
-			
-			$idak=array();
-			if($rows_cont>0){
-				while ($row = $result->fetch_assoc()) {
-					array_push($idak, $row['ID']);
-					
-				}
-			}
-			//Galderen id-ak aleatorioki ordenatu
-			shuffle($idak);
-			$id=$idak[0];
+		include "dbconfig.php"; 
+		$link = mysqli_connect($server, $user, $pass, $db) or die ("Error while connecting to data base.");
+		$puntuazioa = $_GET['puntuazioa'];
+		//Aurreko galderan (egon bada) erantzun zuzena aukeratu bada, puntuazioari bat gehituko diogu
+		if($puntuazioa==1){
+			$link = mysqli_connect($server, $user, $pass, $db) or die ("Error while connecting to data base.");
 			$nick=$_SESSION['nick'];
-		
-			if (empty($_SESSION['galderak'.$nick])) {
-				$_SESSION['galderak'.$nick] = array();
-			}
-			
-			//Konprobatu galdera guztiak erantzun ditugun $_SESSION['galderak'.$nick] egindako galderen id-ak gordetzen goaz 
-			else if(count($_SESSION['galderak'.$nick])==3){
-				echo "<script> alert('Galdera guztiak erantzun dituzu');
-						window.location.assign('finished.php?puntuazioa=0');</script>";
-			} else if(count($_SESSION['galderak'.$nick])==count($idak)){
-				echo "<script> alert('Galdera guztiak erantzun dituzu');
-						window.location.assign('finished.php?puntuazioa=0');</script>";
-			}else{
-				
-				$i=0;
-				$bukatu=false;
-				//Oraindik erantzun ez dugun galderaren id-a bilatzen dugu
-				while(in_array($id, $_SESSION['galderak'.$nick]) and $i<count($idak)){
-					$id=$idak[$i];
-					$i=$i+1;
-				}
-			}
-			//Erantzungo dugun galderaren id-a gordekoa dugu
-			array_push($_SESSION['galderak'.$nick], $id);
-			print_r($_SESSION['galderak'.$nick]);
-			
-			//Galdera hori datu basera eskatu
-			$sql="SELECT * FROM questions WHERE ID='$id'";
+			$_SESSION['zuzenak'] = $_SESSION['zuzenak'] + 1;
+			$sql="UPDATE anonimoak SET puntuazioa=puntuazioa+1 WHERE nick='$nick'";
 			$result=mysqli_query($link, $sql);
-			$row = mysqli_fetch_array($result);
+			if(!($result))
+				echo "Error in the query" . $result->error;
+		}
+		
+		//Galdera guztien id-ak lortu eta array batean sartuko ditugu
+		$sql="SELECT ID FROM questions WHERE gaiarloa='$gaia'";
+		$result=mysqli_query($link, $sql);
+		
+		
+		if(!($result)){
+			echo "Error in the query" . $result->error;
+		} else {
 			$rows_cont = $result->num_rows;
 			if($rows_cont==0){
-				echo "<script> alert('Ez dago id horretako galderarik');</script>";}
+				echo "<script> alert('Ez dago galderarik');</script>";}
 			else{
-				$galdera=$row['testua'];
-				$eZuzen=$row['eZuzen'];
-				//Erantzun guztiak array batean gorde eta aleatorioki ordenatu
-				$erantzunak=array(0=>$row['eZuzen'],1=>$row['eOker1'],2=>$row['eOker2'],3=>$row['eOker3']);
-				shuffle($erantzunak); 
-				//Argazkia duen ikusi, erakusteko ala ez
-				if($row['argazkia']==NULL)
-					$argazkia=false;
-				else
-					$argazkia=$row['argazkia'];
-					$argazkimota=$row['arg_mota'];
+				
+				$idak=array();
+				if($rows_cont>0){
+					while ($row = $result->fetch_assoc()) {
+						array_push($idak, $row['ID']);
+						
+					}
+				}
+				//Galderen id-ak aleatorioki ordenatu
+				shuffle($idak);
+				$id=$idak[0];
+				$nick=$_SESSION['nick'];
+			
+				if (empty($_SESSION['galderak'.$nick])) {
+					$_SESSION['galderak'.$nick] = array();
+				}
+				
+				//Konprobatu galdera guztiak erantzun ditugun $_SESSION['galderak'.$nick] egindako galderen id-ak gordetzen goaz 
+				else if(count($_SESSION['galderak'.$nick])==3){
+					echo "<script> alert('Galdera guztiak erantzun dituzu');
+							window.location.assign('finished.php?puntuazioa=0');</script>";
+				} else if(count($_SESSION['galderak'.$nick])==count($idak)){
+					echo "<script> alert('Galdera guztiak erantzun dituzu');
+							window.location.assign('finished.php?puntuazioa=0');</script>";
+				}else{
+					
+					$i=0;
+					$bukatu=false;
+					//Oraindik erantzun ez dugun galderaren id-a bilatzen dugu
+					while(in_array($id, $_SESSION['galderak'.$nick]) and $i<count($idak)){
+						$id=$idak[$i];
+						$i=$i+1;
+					}
+				}
+				//Erantzungo dugun galderaren id-a gordekoa dugu
+				array_push($_SESSION['galderak'.$nick], $id);
+				
+				//Galdera hori datu basera eskatu
+				$sql="SELECT * FROM questions WHERE ID='$id'";
+				$result=mysqli_query($link, $sql);
+				$row = mysqli_fetch_array($result);
+				$rows_cont = $result->num_rows;
+				if($rows_cont==0){
+					echo "<script> alert('Ez dago id horretako galderarik');</script>";}
+				else{
+					$galdera=$row['testua'];
+					$eZuzen=$row['eZuzen'];
+					//Erantzun guztiak array batean gorde eta aleatorioki ordenatu
+					$erantzunak=array(0=>$row['eZuzen'],1=>$row['eOker1'],2=>$row['eOker2'],3=>$row['eOker3']);
+					shuffle($erantzunak); 
+					//Argazkia duen ikusi, erakusteko ala ez
+					if($row['argazkia']==NULL)
+						$argazkia=false;
+					else
+						$argazkia=$row['argazkia'];
+						$argazkimota=$row['arg_mota'];
+				}
 			}
 		}
 	}
@@ -122,7 +129,6 @@
 
 	<script type="text/javascript">
 	function konprobatu(){
-
 		var elementuak=document.getElementsByName('erantzuna');
 		var luz=elementuak.length;
 		var erantzuna='<?php echo $eZuzen;?>';
@@ -132,7 +138,6 @@
 		
 		//Aukeratu dugun erantzuna bilatu 
 		while( i<luz && !aurkitua){
-
 			if(elementuak[i].checked)
 				aurkitua=true;
 			else i++;
@@ -143,23 +148,19 @@
 		else{
 			eran=elementuak[i].value;
 			if(eran==erantzuna){
-
 					puntuazioa = 1;
 					//Hurrengo orrialdean puntuazioa gehituko diogu
 					window.location.assign('playbysubject.php?puntuazioa=1&subject=<?php echo $gaia;?>');
-
 			} else {
 				
 					window.location.assign('playbysubject.php?puntuazioa=0&subject=<?php echo $gaia;?>');
 				
 			}
-
 			
 			
 		}
 		
     } 
-
 		
 </script>    
 	<section class="main" id="s1">
