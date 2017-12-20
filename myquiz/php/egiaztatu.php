1347230168b50726bff8fea92eb7296e327ec0c7
aps54 <31988318+aps54@users.noreply.github.com>
2017-12-20 18:47:03 +0100

    webhost-en funtzionatzen du
    link-a aldatu behar da webhosteagatik

--- myquiz/php/egiaztatu.php
+++ myquiz/php/egiaztatu.php
@@ -23,6 +23,6 @@ function egiaztatu($pasahitza) {
 	fclose($ireki);
 	return ($emaitza);
 }
-$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
-$server->service($HTTP_RAW_POST_DATA);
+//$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+$server->service(file_get_contents('php://input'));
 ?>
